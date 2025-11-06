0e801ef82e0cb5f66e6d1f07ed5edec33bfb6af4
Arafat Shalehin <md.arafatshalehin@gmail.com>
2025-11-06 15:15:26 +0600

    Some Changes.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./smart-deals-firebase-admin-key.json");
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -229,7 +230,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
